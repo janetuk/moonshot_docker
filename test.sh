a3f237d2fd4badc25226b81822a04cf5e42135c3
Alejandro Perez <alex.perez-mendez@jisc.ac.uk>
2019-10-26 10:01:56 +0200

    dbus-launch is not needed

--- test.sh
+++ test.sh
@@ -7,12 +7,12 @@ docker-compose down -t 0
 docker-compose up --build -d
 
 echo "-----------------Importing User Credentials-----------------------"
-docker-compose exec -e DISPLAY client dbus-launch moonshot-webp /config/user_credentials.xml
+docker-compose exec -e DISPLAY client moonshot-webp /config/user_credentials.xml
 
 echo "-----------------Testing SSH-----------------------"
-docker-compose exec -e DISPLAY client dbus-launch ssh -o StrictHostKeyChecking=no moonshot@sshserver echo "SSH authenticated with moonshot"
+docker-compose exec -e DISPLAY client ssh -o StrictHostKeyChecking=no moonshot@sshserver echo "SSH authenticated with moonshot"
 
 echo "-----------------Testing Apache --------------------"
-docker-compose exec -e DISPLAY client dbus-launch curl --negotiate -u ":" http://httpserver/protected/hello.cgi
+docker-compose exec -e DISPLAY client curl --negotiate -u ":" http://httpserver/protected/hello.cgi
 
 docker-compose stop -t 0
