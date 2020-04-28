bc18862a5b15934f5264c3c4aa9e829797de3008
Alejandro Perez <alex.perez-mendez@jisc.ac.uk>
2020-04-28 11:00:39 +0200

    Tag images appropriately

--- push_docker_images.sh
+++ push_docker_images.sh
@@ -1,10 +1,6 @@
 #!/bin/env bash
-
-for IMAGE in apc trustrouter sshserver httpserver client
+for IMAGE in apc trustrouter sshserver httpserver client idp
 do
-    docker tag moonshot_docker_$IMAGE jiscmoonshot/$IMAGE
     docker push jiscmoonshot/$IMAGE
 done
-docker tag moonshot_docker_idp1 jiscmoonshot/idp
-docker push jiscmoonshot/idp
 
